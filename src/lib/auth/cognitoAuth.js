8f6272ef3f8888191b444a2cd8e45e61dd8a86ea
hassoubeat <hassoubeat.work@gmail.com>
2020-09-10 10:46:13 +0900

    Add Cognito Auth処理の実装

---
+++ src/lib/auth/cognitoAuth.js
@@ -0,0 +1,113 @@
+// AWS Cognitoを利用した認証処理
+import Amplify, { Auth } from 'aws-amplify';
+
+// Amplifyの設定
+Amplify.configure({
+  Auth: {
+    // リージョン
+    region: process.env.REACT_APP_AWS_REGION,
+    // ユーザプールのID
+    userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
+    // アプリクライアントID
+    userPoolWebClientId: process.env.REACT_APP_AWS_COGNITO_CLIENT_ID,
+  }
+});
+
+// サインアップ
+export async function signUp (email, password, attributes, successCallback, errorCallback) {
+  let result = "";
+  try {
+    result = await Auth.signUp({
+      username: email, 
+      password: password,
+      attributes: attributes
+    });
+    if(successCallback) successCallback(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    if(errorCallback) errorCallback(error);
+  }
+};
+
+// 認証コードの検証
+export async function confirmSignUp (email, verifyCode, successCallback, errorCallback) {
+  let result = "";
+  try {
+    result = await Auth.confirmSignUp({
+      username: email,
+      code: verifyCode
+    });
+    if(successCallback) successCallback(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    if(errorCallback) errorCallback(error);
+  }
+}
+
+// 認証コードの再送信
+export async function resendSignUp (email, successCallback, errorCallback) {
+  let result = "";
+  try {
+    result = await Auth.resendSignUp(email);
+    if(successCallback) successCallback(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    if(errorCallback) errorCallback(error);
+  }
+}
+
+// サインイン
+export async function signIn (email, password, successCallback, errorCallback) {
+  let result = "";
+  try {
+    result = await Auth.signIn({
+      username: email, 
+      password: password
+    });
+    if(successCallback) successCallback(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    if(errorCallback) errorCallback(error);
+  }
+};
+
+// IdProvider(Facebook, Google...etc)を利用したサインイン
+export async function federationSignIn (federationProvider, successCallback, errorCallback) {
+  let result = "";
+  try {
+    result = await Auth.federatedSignIn({ provider: federationProvider })
+    if(successCallback) successCallback(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    if(errorCallback) errorCallback(error);
+  }
+};
+
+// サインアウト
+export async function signOut (successCallback, errorCallback) {
+  try {
+    await Auth.signOut();
+    if(successCallback) successCallback(result);
+  } catch (error) {
+    console.log(error);
+    if(errorCallback) errorCallback(error);
+  }
+}
+
+// Password変更
+export async function changePassword (user, oldPassword, newPassword, successCallback, errorCallback) {
+  let result = "";
+  try {
+    result = await Auth.changePassword(user, oldPassword, newPassword)
+    if(successCallback) successCallback(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    if(errorCallback) errorCallback(error);
+  }
+};
