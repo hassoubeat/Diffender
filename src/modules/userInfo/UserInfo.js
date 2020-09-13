1bee3813fc7835b37e0236c0b57ea543bec5b45d
hassoubeat <hassoubeat.work@gmail.com>
2020-09-13 23:42:48 +0900

    Update ユーザ情報編集画面にユーザ属性変更コンポーネントを追加

--- src/modules/userInfo/UserInfo.js
+++ src/modules/userInfo/UserInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UpdateUserAttributes from 'modules/auth/UpdateUserAttributes';
 import ChangePassword from 'modules/auth/ChangePassword';
 import styles from './UserInfo.module.scss';
 
@@ -7,7 +8,11 @@ export default function UserInfo(props = null) {
     <React.Fragment>
       <div className={styles.userInfo}>
         <div className={styles.title}>ユーザ情報の編集</div>
-        <section id="changePassword">
+        <section id="updateUserAttributes" className={styles.section}>
+          <div className={styles.sectionTitle}>ユーザ属性変更</div>
+          <UpdateUserAttributes/>
+        </section>
+        <section id="changePassword" className={styles.section}>
           <div className={styles.sectionTitle}>パスワード変更</div>
           <ChangePassword/>
         </section>
