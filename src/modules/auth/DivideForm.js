11966389c60067f7e9076d544583e8c1e56d4843
hassoubeat <hassoubeat.work@gmail.com>
2020-09-13 14:20:48 +0900

    Update コンテンツの配置場所を左右で選択可能に

--- src/modules/auth/DivideForm.js
+++ src/modules/auth/DivideForm.js
@@ -2,10 +2,20 @@ import React from 'react';
 import styles from './DivideForm.module.scss';
 
 export default function DivideForm(props = null) {
+  // propsの展開
+  const chidrenPosition = props.position || "right";
 
   return (
     <React.Fragment>
       <div className={styles.divideForm}>
+        { (chidrenPosition === "left") && 
+          <div className={styles.block}>
+            <div className={styles.spTitle}>
+              {process.env.REACT_APP_PROJECT_NAME}
+            </div>
+            {props.children}
+          </div>
+        }
         <div className={`${styles.block} ${styles.appInfo}`}>
           <div className={styles.title}>
             {process.env.REACT_APP_PROJECT_NAME}
@@ -18,12 +28,14 @@ export default function DivideForm(props = null) {
             <img className={styles.image} src="/img/bgSignInLandScape.png" alt="diff" />
           </div>
         </div>
-        <div className={styles.block}>
-          <div className={styles.spTitle}>
-            {process.env.REACT_APP_PROJECT_NAME}
+        { (chidrenPosition === "right") && 
+          <div className={styles.block}>
+            <div className={styles.spTitle}>
+              {process.env.REACT_APP_PROJECT_NAME}
+            </div>
+            {props.children}
           </div>
-          {props.children}
-        </div>
+        }
       </div>
     </React.Fragment>
   );
