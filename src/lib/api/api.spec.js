e42a7236f08459346f3baba3d4970b3be7135882
hassoubeat <hassoubeat.work@gmail.com>
2020-09-16 16:16:24 +0900

    Update Cognito認証を単体テストで対応するための設定追加

--- src/lib/api/api.spec.js
+++ src/lib/api/api.spec.js
@@ -1,16 +1,28 @@
 import * as api from "./api";
+import { signIn } from 'lib/auth/cognitoAuth';
+
+// テスト用のユーザID、パスワード取得
+const TEST_CONGNITO_USER_ID = process.env.REACT_APP_TEST_COGNITO_USER_ID;
+const TEST_CONGNITO_USER_PASSWORD = process.env.REACT_APP_TEST_COGNITO_USER_PASSWORD;
 
 // 環境変数設定
 // 正常系は実際のデータベースに値の変更を加えるため、デフォルトスキップ
 describe.skip('API呼び出し処理 正常系のテスト群', () => {
+
+  beforeAll( async () => {
+    // サインイン
+    await signIn(
+      TEST_CONGNITO_USER_ID,
+      TEST_CONGNITO_USER_PASSWORD
+    );
+  });
 
   test('プロジェクト登録', async () => {
     const data = {
       body: {
         "project": {
           "name": "プロジェクト1",
-          "description": "テストプロジェクトです",
-          "projectTieUserId": "48baa275-3880-48a2-98a1-b5089aa98386"
+          "description": "テストプロジェクトです"
         }
       }
     }
@@ -25,8 +37,7 @@ describe('API呼び出し処理 異常系テスト群', () => {
       body: {
         "project": {
           "name": "1234512345123451234512345123451",
-          "description": "テストプロジェクトです",
-          "projectTieUserId": "48baa275-3880-48a2-98a1-b5089aa98386"
+          "description": "テストプロジェクトです"
         }
       }
     }
