2de98a43072e915d133ebf1a81b20749b78583bb
hassoubeat <hassoubeat.work@gmail.com>
2020-10-02 13:22:33 +0900

    Update PageFormのReactHookForm対応
    複雑な入力フォームであるため、ReactHookFormを導入
    
    合わせていくつか改善対応
    ・useContextを利用してPageオブジェクトの引き回しをしないように

--- src/components/page/pageForm/_BrowserSettingsForm.js
+++ src/components/page/pageForm/_BrowserSettingsForm.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useFormContext } from "react-hook-form";
+import { PageContext } from './PageForm';
 import styles from './_BrowserSettingsForm.module.scss';
 
 export default function BrowserSettingsForm(props = null) {
   // パラメータ取得
-  const {browserSettings, setBrowserSettings, deviceList} = props.payload;
+  const deviceList = props.deviceList;
+
+  // Hook setup
+  const {page} = useContext(PageContext);
+  const {register, errors, watch} = useFormContext();
+
+  // ReactHookForm watchs
+  const deviceType = watch("browserSettings.deviceType");
 
   return (
     <React.Fragment>
@@ -13,17 +22,50 @@ export default function BrowserSettingsForm(props = null) {
             デバイスタイプ
           </label>
           <div>
-            <select className={styles.inputSelect} type="select"
-              onChange={(e) => { 
-                browserSettings.deviceType = e.target.value;
-                setBrowserSettings(Object.assign({}, browserSettings));
-              }
-            } >
-              { deviceList.map( (device) => (　<option key={device} value={device} checked={(browserSettings.deviceType === device)}>{device}</option>　)) }
+            <select 
+              name="browserSettings.deviceType"
+              className={styles.inputSelect} 
+              type="select" 
+              defaultValue={page.browserSettings.deviceType}
+              ref={register({
+                required: 'デバイスタイプは必須です'
+              })}
+            >
+            { deviceList.map( (device) => (　
+              <option key={device} value={device} >
+                {device}
+              </option>
+              )
+            )}
             <option value="custom"> カスタマイズ </option>
           </select>
+          {
+            errors.browserSettings && 
+            <div>
+              {errors.browserSettings.deviceType.message}
+            </div>
+          }
           </div>
         </div>
+        {
+          (deviceType === "custom") &&
+          <div className={styles.inputItem}>
+            ※ TODO 画面サイズ指定
+            <select 
+              name="browserSettings.deviceSize"
+              className={styles.inputSelect} 
+              type="select" 
+              defaultValue="1200x1080"
+              ref={register({
+                required: 'デバイスサイズは必須です'
+              })}
+            >
+              <option value="1200x1080" >1080x900</option>
+              <option value="1080x900" >1080x900</option>
+            </select>
+          </div>
+        }
+        
       </div>
     </React.Fragment>
   );
--- src/components/page/pageForm/_ScreenshotOptionsForm.js
+++ src/components/page/pageForm/_ScreenshotOptionsForm.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useFormContext } from "react-hook-form";
+import { PageContext } from './PageForm';
 import styles from './_ScreenshotOptionsForm.module.scss';
 
 export default function ScreenshotOptionsForm(props = null) {
-  // パラメータ取得
-  const {screenshotOptions, setScreenshotOptions} = props.payload;
+
+  // Hook setup
+  const {page} = useContext(PageContext);
+  const {register} = useFormContext();
 
   return (
     <React.Fragment>
       <div className={styles.screenshotOptionsForm}>
-          <input className={styles.checkBox} type="checkBox" checked={!!screenshotOptions.fullPage} onChange={
-            (e) => { 
-              screenshotOptions.fullPage = e.target.checked;
-              setScreenshotOptions(Object.assign({}, screenshotOptions));
-            }
-          } />全画面撮影
+          <input 
+            name="screenshotOptions.fullPage"
+            className={styles.checkBox} 
+            type="checkBox" 
+            defaultChecked={!!page.screenshotOptions.fullPage} 
+            ref={register}
+          />全画面撮影
       </div>
     </React.Fragment>
   );
