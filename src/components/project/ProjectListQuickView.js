889a1393528e6c200f1f91f59880980686bc990e
hassoubeat <hassoubeat.work@gmail.com>
2020-10-20 22:38:55 +0900

    Add プロジェクト一覧クイックアクセスコンポーネントの実装

---
+++ src/components/project/ProjectListQuickView.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { 
+  selectCurrentUserOption
+} from 'app/userSlice';
+
+import { 
+  selectProjects
+} from 'app/domainSlice';
+
+import _ from 'lodash';
+import {
+  sortProjectList
+} from 'lib/project/model';
+import {
+  getLSItem,
+  setLSItem,
+  toBoolean
+} from 'lib/util/localStorage'
+import styles from './ProjectListQuickView.module.scss';
+
+export default function ProjectListQuickView(props = null) {
+  // props setup
+  const selectedProjectId = props.selectedProjectId;
+
+  // hook setup
+  const history = useHistory();
+
+  // redux-state setup
+  const userOption = _.cloneDeep(useSelector(selectCurrentUserOption));
+  const projectsSortMap = userOption.projectsSortMap || {};
+  const projectList = sortProjectList(
+    _.cloneDeep(useSelector(selectProjects)),
+    projectsSortMap
+  );
+
+  // state setup
+  const [isDisplayMenu, setIsDisplayMenu] = useState(
+    toBoolean(getLSItem('isDisplayProjectQuickMenu', false)) 
+  );
+
+  // メニュー表示切り替え
+  const handleDisplayMenuToggle = () => {
+    setLSItem('isDisplayProjectQuickMenu', !isDisplayMenu);
+    setIsDisplayMenu(!isDisplayMenu);
+  }
+
+  return (
+    <React.Fragment>
+      {isDisplayMenu && 
+        <div className={`${styles.projectListQuickView}`}>
+          <div 
+            className={styles.closeMenuItem}
+            onClick={ () => { 
+              handleDisplayMenuToggle()
+            }}
+          >
+           <i className="fas fa-angle-double-left" /> close
+          </div>
+          {projectList.map( (project) => (
+              <div 
+                key={project.id}
+                id={project.id} 
+                className={`
+                  ${styles.projectItem} 
+                  ${(project.id === selectedProjectId) && styles.selected}
+                `}
+                onClick={() => { 
+                  history.push(`/projects/${project.id}`)}
+                }
+              >
+                <div className={styles.main}>
+                  <span className={styles.title}>
+                    {project.name}
+                  </span>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      }
+      {!isDisplayMenu && 
+        <div className={`${styles.closeProjectListQuickView}`} onClick={() => {
+          handleDisplayMenuToggle();
+        }}>
+          <i className="fas fa-angle-double-right" />
+          <div className={styles.label}>
+            Project
+          </div>
+        </div>
+      }
+    </React.Fragment>
+  );
+}
