25dede0fec943a622c9e0993c8de5eb98b2af044
hassoubeat <hassoubeat.work@gmail.com>
2020-10-20 18:04:18 +0900

    Add スクリーンショット取得処理がタイムアウトしたときのDLQ用の処理を実装

---
+++ lambda/screenshots/ScreenshotProcessDLQFunction/index.js
@@ -0,0 +1,17 @@
+const lambdaCommon = require('lambda-common');
+const resultItemDao = require('result-item-dao');
+
+exports.lambda_handler = async (event, context) => {
+  const queueData = lambdaCommon.getSQSRecord(event);
+  const resultItem = queueData.resultItem;
+
+  await resultItemDao.updateResultItem({
+    ...resultItem,
+    status: {
+      type: "ERROR",
+      message: "Screenshots shooting failed.",
+      errorDetailMessage: "Lambda run time exceeded."
+    }
+  });
+  return "finish.";
+};
