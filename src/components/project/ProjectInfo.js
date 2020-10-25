f20ec9a41b90d8e5fff150ec535681536475953d
hassoubeat <hassoubeat.work@gmail.com>
2020-10-25 19:03:15 +0900

    Update プロジェクト詳細ページの編集
    ・スクリーンショットの取得メニューを詳細画面から削除
    ・古いスクリーンショットリクエストフォームコンポーネントを削除

--- src/components/project/ProjectInfo.js
+++ src/components/project/ProjectInfo.js
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import Modal from 'react-modal';
 import ProjectForm from './ProjectForm';
-import ScreenshotRequestForm from './ScreenshotRequestForm';
 import ResultList from 'components/result/ResultList';
 import styles from './ProjectInfo.module.scss';
-
-// モーダルの展開先エレメントの指定
-Modal.setAppElement('#root');
 
 export default function ProjectInfo(props = null) {
   // props setup
@@ -16,18 +11,13 @@ export default function ProjectInfo(props = null) {
   // hook setup
   const history = useHistory();
 
-  // state setup
-  const [isDisplayScreenshotRequestFormModal, setIsDisplayScreenshotRequestFormModal] = useState(false);
-
   return (
     <React.Fragment>
       <div className={styles.projectInfo}>
+        <div className="sectionTitle">プロジェクト</div>
         <div className={styles.actionArea}>
-          <button className={`button ${styles.screenshotRequestButton}`} onClick={
-            async () => { setIsDisplayScreenshotRequestFormModal(true) }
-          }>スクリーンショットの取得</button>
           <button className={`button ${styles.addPageButton}`} onClick={() => {history.push(`/projects/${projectId}/pages`)} }>
-            ページの追加
+            ページ一覧
           </button>
         </div>
         <ProjectForm 
@@ -41,21 +31,6 @@ export default function ProjectInfo(props = null) {
           <ResultList projectId={projectId} isDisplayDiffRequestForm={true} />
         </div>
       </div>
-      <Modal 
-        isOpen={isDisplayScreenshotRequestFormModal}
-        onRequestClose={() => {setIsDisplayScreenshotRequestFormModal(false)}}
-        className="modalContent"
-        overlayClassName="modalOverray"
-      >
-        <div className="modalTitle">スクリーンショットを取得する</div>
-        <small className="modalSupportMessage">
-          現在のプロジェクトに登録されているページのスクリーンショットを取得します。<br/>
-          スクリーンショットの取得結果は”リザルト”として登録されます。<br/>
-          ※ ページ数が多い場合、完了まで時間がかかります
-        </small>
-        <ScreenshotRequestForm projectId={projectId} requestSuccessCallback={() => {setIsDisplayScreenshotRequestFormModal(false)}} />
-        <div className="closeModalButton" onClick={() => {setIsDisplayScreenshotRequestFormModal(false)}}>✕</div>
-      </Modal>
     </React.Fragment>
   );
 }
