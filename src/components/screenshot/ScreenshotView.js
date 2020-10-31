70d6c0245506ab30f4c1eb258d19df0da59e348e
hassoubeat <hassoubeat.work@gmail.com>
2020-10-31 11:22:37 +0900

    Update 画像読み込み時のイベントを設定
    ・読込中にローディング表示
    ・読み込み失敗時にエラー表示

--- src/components/screenshot/ScreenshotView.js
+++ src/components/screenshot/ScreenshotView.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Loading from 'components/common/Loading';
 import styles from './ScreenshotView.module.scss';
 
 export default function ScreenshotView(props = null) {
@@ -6,12 +7,47 @@ export default function ScreenshotView(props = null) {
   const screenshotURL = props.screenshotURL;
 
   // state setup
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoadError, setIsLoadError] = useState(false);
   const [screenshotZoom, setScreenshotZoom] = useState(false);
+
+  // 画像の読み込みが終わった時に
+  const img = new Image();
+  img.src = screenshotURL;
+  img.onload = () => {
+    setIsLoaded(true);
+  }
+  img.onerror = () => {
+    setIsLoadError(true);
+  }
+
+  // 画像が変わる度にローディング状況をリセットする
+  useEffect( () => {
+    setIsLoaded(false);
+    setIsLoadError(false);
+  }, [screenshotURL]);
+  
+
+  if (!screenshotURL) {
+    return (
+      <div className={styles.notfound}>
+        スクリーンショットがありません
+      </div>
+    )
+  }
+
+  if (isLoadError) {
+    return (
+      <div className={styles.notfound}>
+        画像の読み込みに失敗しました
+      </div>
+    )
+  }
 
   return (
     <React.Fragment>
       <div className={styles.screenshotView}>
-        { screenshotURL ? 
+        { (isLoaded) ?
           <img 
             className={`${styles.screenshot} ${(screenshotZoom) ? styles.zoom : ""}`}
             src={screenshotURL} 
@@ -19,9 +55,9 @@ export default function ScreenshotView(props = null) {
             onClick={() => {
               setScreenshotZoom(!screenshotZoom)
             }}
-          /> : 
+          /> :
           <div className={styles.notfound}>
-            スクリーンショットがありません
+            <Loading/>
           </div>
         }
       </div>
