f612f5fe46782b51456be44bffd2106386262531
hassoubeat <hassoubeat.work@gmail.com>
2020-11-03 22:17:25 +0900

    Add プロジェクト削除時に関連データの削除も実行する非同期処理を実装

--- lambda/projects/DeleteProjectFunction/index.js
+++ lambda/projects/DeleteProjectFunction/index.js
@@ -1,6 +1,12 @@
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
+
 const jwt_decode = require('jwt-decode');
 const projectDao = require('project-dao');
+const pageDao = require('page-dao');
 const lambdaCommon = require('lambda-common');
+
+const ASYNC_QUEING_LAMBDA_NAME = process.env.ASYNC_QUEING_LAMBDA_NAME;
 
 exports.lambda_handler = async (event, context) => {
   // レスポンス変数の定義
@@ -24,6 +30,17 @@ exports.lambda_handler = async (event, context) => {
       resouceUserId: project.projectTieUserId
     })
 
+    // 紐付いているページの削除
+    // 紐付いているページが多いと完了まで時間がかかるため、非同期で別のLambdaを利用して処理を実行
+    const params = {
+      FunctionName: ASYNC_QUEING_LAMBDA_NAME,
+      InvocationType:"Event",
+      Payload: JSON.stringify({
+        project: project
+      })
+    }
+    await lambda.invoke(params).promise();
+
     await projectDao.deleteProject(projectId);
 
     response.body = JSON.stringify(project);
@@ -36,4 +53,22 @@ exports.lambda_handler = async (event, context) => {
     });
   }
   return response;
+}
+
+// 実行に時間がかかる関連データの削除処理(紐付いているページ)
+// 別のLambdaとして登録して非同期でメイン処理からコールする
+exports.async_delete_handler= async (event, context) => {
+  const project = event.project;
+
+  try {
+    const pageList = await pageDao.getPageList(project.id);
+
+    for(const page of pageList) {
+      // ページの削除
+      await pageDao.deletePage(page.id);
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
 }
